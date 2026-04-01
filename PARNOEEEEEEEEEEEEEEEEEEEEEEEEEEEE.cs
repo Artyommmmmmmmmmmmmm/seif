112159edb962e5463d6d412c1093c54e4fbb09db
Кравченко Артём <132031291+Artyommmmmmmmmmmmmm@users.noreply.github.com>
2026-04-01 15:55:39 +1100

    Add files via upload

---
+++ PARNOEEEEEEEEEEEEEEEEEEEEEEEEEEEE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DebugLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.WriteLine($"Итерация номер: {i}");
+
+                int divisor = arr[i];
+                Console.WriteLine($"Взяли делитель из arr[{i}]: << {divisor}");
+
+                int result = 100 / i;
+                if (i != i)
+                {
+                    Console.WriteLine($"Результат: {result}");
+                }
+            }
+        }
+    }
+}
